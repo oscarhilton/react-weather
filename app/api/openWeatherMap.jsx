0882af8d7979c53825b9bc967cbb03e218701645
oscarhilton <oscarhilton@hotmail.com>
2016-12-18 23:57:50 +0000

    first commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+var axios = require("axios");
+
+var key = '9ae3eba429d2830ce803d555824e5fad';
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=' + key + '&units=metric';
+
+module.exports = {
+    
+    getTemp: function (location){
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+        
+        return axios.get(requestUrl).then(function(res){
+            if(res.data.cod && res.data.message){
+                throw new Error(res.data.message);
+            } else {
+                return res.data.main.temp;
+            }
+        }, function(res) {
+            throw new Error(res.data.message);
+        });
+    }
+}
---
+++ app/components/Weather.jsx
@@ -0,0 +1,40 @@
+var React = require("react");
+var WeatherForm = require("WeatherForm");
+var WeatherMessage = require("WeatherMessage");
+var OpenWeatherMap = require("OpenWeatherMap");
+
+var Weather = React.createClass({
+    getInitialState: function(){
+        return{
+            location: 'Miami',
+            temp: 88
+        }
+    },
+    handleSearch: function(location){
+        var that = this;
+        
+        OpenWeatherMap.getTemp(location).then(function(temp){
+            that.setState({
+                location: location,
+                temp: temp
+            })
+        }, function(errorMessage){
+            alert(errorMessage);
+        });
+
+    },
+    render: function () {
+        
+        var {temp, location} = this.state;
+        
+        return (
+            <div>
+                <h1>Get Weather</h1>
+                <WeatherForm onSearch={this.handleSearch}/>
+                <WeatherMessage location={location} temp={temp}/>
+            </div>
+    );
+   } 
+});
+
+module.exports = Weather;
